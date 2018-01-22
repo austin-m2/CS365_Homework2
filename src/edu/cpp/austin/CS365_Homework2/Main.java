82c640e37dc2a9622d2c10a101f08459c7529dae
austin-m2 <morris.austinm@gmail.com>
2018-01-22 13:08:02 -0800

    integer addition working (in one case)

--- src/edu/cpp/austin/CS365_Homework2/Main.java
+++ src/edu/cpp/austin/CS365_Homework2/Main.java
@@ -7,6 +7,11 @@ public class Main {
         int a = 0x42c923d7; // 100.57
         int b = 0x3bf227d0; // 0.00739
 
-	    fp.add(a, b);
+        int v24_25 = 0x41C20000; //  24.25
+        int v_1875 = 0xBE400000; // -0.1875
+        int v5     = 0xC0A00000; // -5.0
+
+	    int c = fp.add(v24_25, v24_25);
+	    System.out.println(c);
     }
 }
--- src/edu/cpp/austin/CS365_Homework2/fp.java
+++ src/edu/cpp/austin/CS365_Homework2/fp.java
@@ -2,13 +2,14 @@ package edu.cpp.austin.CS365_Homework2;
 
 public class fp {
     public int add(int a, int b) {
-        //put the number with smaller exponent in a
+        //put the number with smaller exponent in a (to make it easier for me)
         if (((a >> 23) & 255) > ((b >> 23) & 255)) {
             int temp = a;
             a = b;
             b = temp;
         }
 
+        //split numbers into sign, exponent, and fraction
         int aS = (a >> 31) & 1;
         int aE = (a >> 23) & 255;
         int aF =  a & 0x7FFFFF;
@@ -17,29 +18,45 @@ public class fp {
         int bE = (b >> 23) & 255;
         int bF =  b & 0x7FFFFF;
 
-        //add back the 1 in front of the decimal point, shift to the right
+        //add back the 1 in front of the decimal point, (increment exponent?)
         //numbers are now in the form 0.x
         aF += 0x800000;
         bF += 0x800000;
-        aE += 1;
-        bE += 1;
 
         //shifts a to the right until exponents match
         aF = aF >> (bE - aE);
         aE = bE;
 
-        //add the numbers together
+        //add the significands together
         int cF = aF + bF;
-        int cE = aE;
 
-        //normalize the sum
-        int shiftAmt = Integer.numberOfLeadingZeros(cF) + 1;
-        cF = cF << shiftAmt;
-        cE -= shiftAmt;
+        //we need 8 leading zeroes for a properly normalized sum
+        //(int is 32 bits; result should be 8 zeroes and a 1, followed by 23 bit mantissa)
+        int shiftAmt = 8 - Integer.numberOfLeadingZeros(cF);
 
+        //shift so that "hidden 1 bit" is in the correct place
+        if (shiftAmt >= 0) {
+            cF = cF >> shiftAmt;
+        } else {
+            cF = cF << Math.abs(shiftAmt);
+        }
 
+        //remove the hidden bit
+        cF -= 0x800000;
 
-        return 0;
+        //set the add the shift amount to the exponent
+        int cE = aE + shiftAmt;
+
+        //fix! incorrect! TODO! assumes that sign doesn't change
+        int cS = aS;
+
+        //reassemble integer result
+        int c = cF;
+        c += cE << 23;
+        c += cS << 31;
+
+        return c;
+
     }
 
     public int mul(int a, int b) {
